dccad21f669b6f2916fe94b681d38ccf70e17b96
Sebastian Struß <struss@justtrack.io>
2023-01-27 15:39:01 +0100

    feat: allow usage of enabled flag for resource creation

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "queue_name" {
   description = "SQS queue name"
-  value       = try(aws_sqs_queue.queue[0].name, aws_sqs_queue.kms_encrypted_queue[0].name)
+  value       = try(aws_sqs_queue.queue[0].name, aws_sqs_queue.kms_encrypted_queue[0].name, "")
 }
 
 output "queue_arn" {
   description = "SQS queue arn"
-  value       = try(aws_sqs_queue.queue[0].arn, aws_sqs_queue.kms_encrypted_queue[0].arn)
+  value       = try(aws_sqs_queue.queue[0].arn, aws_sqs_queue.kms_encrypted_queue[0].arn, "")
 }
 
 output "queue_id" {
   description = "SQS queue id"
-  value       = try(aws_sqs_queue.queue[0].id, aws_sqs_queue.kms_encrypted_queue[0].id)
+  value       = try(aws_sqs_queue.queue[0].id, aws_sqs_queue.kms_encrypted_queue[0].id, "")
 }
